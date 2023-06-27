2ae69cc5a964aa5b6f4af12693c677683fd7707a
Grant Ramsay <seapagan@gmail.com>
2023-06-27 09:27:47 +0100

    refactor existing plugin code slightly
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- mkdocs_run_shell_cmd_plugin/plugin.py
+++ mkdocs_run_shell_cmd_plugin/plugin.py
@@ -5,10 +5,18 @@ import subprocess  # nosec
 from typing import Literal
 
 from mkdocs.plugins import BasePlugin
+from rich import print  # pylint: disable=redefined-builtin
 
 
 class RunShellCmdPlugin(BasePlugin):
     """Class for the 'mkdocs-run-shell-cmd-plugin'."""
+
+    def __init__(self):
+        """Set up variables for this plugin."""
+        self.plugin_tag = "run-shell-cmd"
+
+        self.always_run = False
+        self.disable_run = False
 
     def ok_to_run(self, command: str):
         """Return True if we should run on this command.
@@ -17,7 +25,7 @@ class RunShellCmdPlugin(BasePlugin):
         """
         if self.disable_run:
             return False
-        elif self.always_run:
+        if self.always_run:
             return True
 
         run_this = input(
@@ -29,16 +37,12 @@ class RunShellCmdPlugin(BasePlugin):
         elif run_this.lower() in ["d", "disable"]:
             self.disable_run = True
 
-        return (
-            True
-            if (
-                run_this == ""
-                or run_this.lower() in ["y", "yes"]
-                or (
-                    self.always_run and not self.disable_run
-                )  # disable has priority, should never be set together anyway.
-            )
-            else False
+        return bool(
+            run_this == ""
+            or run_this.lower() in ["y", "yes"]
+            or (
+                self.always_run and not self.disable_run
+            )  # disable has priority, should never be set together anyway.
         )
 
     def on_startup(
@@ -50,16 +54,12 @@ class RunShellCmdPlugin(BasePlugin):
         to the new system where the plugin object is kept across builds within
         one mkdocs serve.
         """
-        self.PLUGIN_TAG = "run-shell-cmd"
-
-        self.always_run = False
-        self.disable_run = False
 
     def on_page_markdown(self, markdown: str, **kwargs) -> str | None:
         """Modify the markdown if our tag is present."""
         cmds = re.finditer(
             pattern=(
-                rf"{{{{\s*{self.PLUGIN_TAG}"
+                rf"{{{{\s*{self.plugin_tag}"
                 rf"\(cmd=[\"|\'](.+)[\"|\']\).*\s*}}}}"
             ),
             string=markdown,
@@ -77,14 +77,21 @@ class RunShellCmdPlugin(BasePlugin):
                         check=True,
                     )  # nosec
                     cmd_result = cmd_output.stdout
-                except subprocess.CalledProcessError as e:
-                    cmd_result = e.stderr
-                    print(e)
+                except subprocess.CalledProcessError as exc:
+                    cmd_result = exc.stderr
+                    print(exc)
 
                 output = (
                     "\n\n```{.console linenums='0' title=''}"
                     f"\n$ {match.group(1)}\n{cmd_result}\n```\n"
                 )
-                markdown = markdown.replace(match.group(0), output)
+                # we only replace the first occurrence of the tag, in case
+                # multiple tags are present in the markdown. This allows
+                # commands that have different output when run again to be
+                # shown. It would slow the build down if the same command is run
+                # multiple times without output changing but that is acceptable.
+                # Maybe we can add a config option to control this in the
+                # future.
+                markdown = markdown.replace(match.group(0), output, 1)
 
         return markdown
